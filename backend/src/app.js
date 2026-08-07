8f80edde9dd78c37ea434de7ed54a3972131b06f
Abdullah Ejaz <abdullahraj982@gmail.com>
2026-08-07 15:31:39 +0500

    chore: Removed backend docs(README.md) from frontend PR

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,9 @@ import express from "express";
 import loggerMiddleware from "./middleware/loggerMiddleware.js";
 import authRoutes from "./routes/authRoutes.js";
 import notesRoutes from "./routes/notesRoutes.js";
-import cors from "cors";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true
-  }),
-);
 app.use(express.json());
 app.use(loggerMiddleware);
 
